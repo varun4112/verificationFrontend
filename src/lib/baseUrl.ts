eb225f1f95b3005a47e58af8daeef749b93dd0ef
varun <varunnidumm@gmail.com>
2024-09-01 19:39:33 +0530

    first

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,6 +1,6 @@
 // baseurl.ts
-// const BASE_URL = 'https://verification-2iqj.onrender.com';
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = 'https://verification-2iqj.onrender.com';
+// const BASE_URL = 'http://localhost:4000';
 
 
 export default BASE_URL;
